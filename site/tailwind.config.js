0abe0b82b643c4dfa9a6a82216e2d71c6093d52d
Mihusu <ming2200@hotmail.com>
2024-03-19 10:34:24 +0100

    Initial draft

---
+++ site/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          cream: '#EFE9F4',
+          bluecity: '#0093d2',
+          bluecity_dark: '#004981'
+        }
+      }
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+      prefix: "",
+      darkTheme: "dark",
+    },
+  };
+  
