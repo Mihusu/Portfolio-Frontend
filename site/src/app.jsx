5718546c1ae3395ccdd20bffa2a2785d27643410
Mihusu <ming2200@hotmail.com>
2024-04-08 10:51:55 +0200

    Initial draft

---
+++ site/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import About from './pages/about';
+// import Certifications from './pages/certifications';
+// import Contact from './pages/contact';
+// import Educations from './pages/educations';
+// import Experiences from './pages/experiences';
+// import Footer from './pages/Footer';
+// import Greeting from './pages/greeting';
+// import Header from './pages/header';
+// import Projects from './pages/projects';
+
+function App() {
+  
+    return (
+        <div> Something</div>
+    );
+}
+
+export default App
