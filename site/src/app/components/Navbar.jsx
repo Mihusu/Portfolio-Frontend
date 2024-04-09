131dc3fa8d2535b1b2154a6fe6040f95ae85fac1
Mihusu <ming2200@hotmail.com>
2024-04-09 09:42:56 +0200

    Update Navbar.jsx

--- site/src/app/components/Navbar.jsx
+++ site/src/app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import NavLink from './NavLink';
 import MenuOverlay from './MenuOverlay';
@@ -22,6 +22,22 @@ const navLinks = [
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (window.innerWidth >= 768) {
+        setNavbarOpen(false);
+      }
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
@@ -32,27 +48,29 @@ const Navbar = () => {
         >
           LOGO
         </Link>
-        <div className='mobile-menu block md:hidden'>
-          {
-            !navbarOpen ? (
-              <button 
-                onClick={() => setNavbarOpen(true)} 
-                className='flex items-center px-3 py-2 border rounded border-slate-200 text slate-200 hover:text-white'
-              >
-                <Bars3Icon className="h-5 w-5"/>
-              </button>
-            ) : (
-              <button 
-                onClick={() => setNavbarOpen(false)} 
-                className='flex items-center px-3 py-2 border rounded border-slate-200 text slate-200 hover:text-white'
-              >
-                <XMarkIcon className="h-5 w-5"/>
-              </button>
-            )
-          }
-        </div>
-        <div className={`menu hidden md:block md:w-auto`} id="Navbar">
-          <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+        {windowWidth < 768 && (
+          <div className='mobile-menu block md:hidden'>
+            {
+              !navbarOpen ? (
+                <button 
+                  onClick={() => setNavbarOpen(true)} 
+                  className='flex items-center px-3 py-2 border rounded border-slate-200 text slate-200 hover:text-white'
+                >
+                  <Bars3Icon className="h-5 w-5"/>
+                </button>
+              ) : (
+                <button 
+                  onClick={() => setNavbarOpen(false)} 
+                  className='flex items-center px-3 py-2 border rounded border-slate-200 text slate-200 hover:text-white'
+                >
+                  <XMarkIcon className="h-5 w-5"/>
+                </button>
+              )
+            }
+          </div>
+        )}
+        <div className={`menu ${windowWidth >= 768 ? 'block' : 'hidden'} md:block md:w-auto`} id="Navbar">
+          <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8'>
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title}/>
